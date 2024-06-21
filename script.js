1d741fe0e975cc6d22e4b0812ba76eec63b0b95f
Буряк Мария <mary.bureak@gmail.com>
2024-06-21 14:36:08 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Переход на страницу каталога при нажатии на кнопку
+document.getElementById('catalogButton').addEventListener('click', function() {
+    window.location.href = 'catalog.html';
+});
